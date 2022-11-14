e7574d4de2f1c9ddeb0f2dbde2bfa23f2699eccb
Pedro Vallejo <p.vallejo@uniandes.edu.co>
2022-11-14 09:58:11 -0500

    Write basic erc20 contract
    Now the server on the route api/erc20 can write a basic erc 20 file on the contracts directory

--- server/controllers/erc20.js
+++ server/controllers/erc20.js
@@ -1,20 +1,21 @@
-const { request, response } = require('express');
+const { request, response } = require( 'express' );
 const { exec } = require( 'child_process' );
+const fs = require( 'fs' );
+const path = require( 'path' );
 
 const erc20Get = (req = request, res = response) => {
     
-    exec("truffle migrate", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-    
+    // exec("truffle migrate", (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.log(`error: ${error.message}`);
+    //         return;
+    //     }
+    //     if (stderr) {
+    //         console.log(`stderr: ${stderr}`);
+    //         return;
+    //     }
+    //     console.log(`stdout: ${stdout}`);
+    // });
     
     res.json({
         msg : 'get API - controller'
@@ -29,6 +30,22 @@ const erc20Put = (req = request, res = response) => {
 
 const erc20Post = (req = request, res = response) => {
     const body = req.body;
+
+    const contractTemplate =
+    `// SPDX-License-Identifier: MIT\n` +
+    `pragma solidity ^0.8.9;\n` +
+    `\n` +
+    `import "@openzeppelin/contracts/token/ERC20/ERC20.sol";\n` +
+    `\n` +
+    `contract MyToken is ERC20 {\n` +
+    `    constructor() ERC20("MyToken", "MTK") {}\n` +
+    `}`
+
+    fs.writeFile(path.join(__dirname, '..', 'contracts', 'MyToken.sol'), contractTemplate, (err) => {
+        if (err) throw err;
+
+        console.log('Write complete')
+    })
     
     res.json({
         msg : 'post API - controller',
