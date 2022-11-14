af33ac4deaea9eb2a79c95e27f5bb46fddab00de
Pedro Vallejo <p.vallejo@uniandes.edu.co>
2022-11-14 14:55:47 -0500

    Deployement file is created based on parameters of request
    When the user sends a post request to the endpoint 'api/erc20' now the application will write a deployment file for the contract on the migrations directory

--- server/controllers/erc20.js
+++ server/controllers/erc20.js
@@ -29,29 +29,35 @@ const erc20Put = (req = request, res = response) => {
     });
 }
 
+/**
+ * Controller for the post request of an ERC-20 token
+ * @param {*} req Request to the server
+ * @param {*} res The response from the server
+ */
 const erc20Post = (req = request, res = response) => {
+    // Request body
     const { name, symbol, tokenAmount } = req.body;
 
-    const contractTemplate =
-    `// SPDX-License-Identifier: MIT\n` +
-    `pragma solidity ^0.8.9;\n` +
-    `\n` +
-    `import "@openzeppelin/contracts/token/ERC20/ERC20.sol";\n` +
-    `\n` +
-    `contract ${name} is ERC20 {\n` +
-    `    constructor() ERC20("${name}", "${symbol}") {\n` +
-    `        _mint(msg.sender, ${tokenAmount} * 10 ** decimals());\n` +
-    `    }\n` +
-    `}`;
+    // Filename for the solidity contract
+    const fileName = `ERC-20:${uniqid()}`;
 
-    const fileName = `${uniqid()}`
+    // Templates needed for deployement and contract creation
+    const deployementFile = deployementFileTemplate(name, fileName);
+    const contractTemplate = erc20ContractTemplate(name, symbol, tokenAmount);
 
-    fs.writeFile(path.join(__dirname, '..', 'contracts', `ERC-20:${fileName}.sol`), contractTemplate, (err) => {
+    // Creation of the needed files
+    fs.writeFile(path.join(__dirname, '..', 'migrations', '1_deploy_contracts.js'), deployementFile, (err) => {
         if (err) throw err;
 
-        console.log('Write complete')
-    })
+        console.log(`${new Date().toISOString}:`,`The deployement file in order to deploy the token ${name} was succesfully updated`);
+        fs.writeFile(path.join(__dirname, '..', 'contracts', `${fileName}.sol`), contractTemplate, (err) => {
+            if (err) throw err;
     
+            console.log(`${new Date().toISOString}:`,`The solidity file for the token ${name} was succesfully written`);
+        });
+    });
+    
+    // Response of the server
     res.json({
         msg : `The token ${name} with the symbol ${symbol} has been created with a total amount of ${tokenAmount} tokens`
     });
@@ -63,10 +69,49 @@ const erc20Delete = (req = request, res = response) => {
     });
 }
 
+/**
+ * Creates a string for a deployement file from a template and the parameters specified
+ * @param {*} tokenName The name of the token
+ * @param {*} fileName  The name of the file where the solidity contract is stored
+ * @returns A string with the content of the deployement file
+ */
+function deployementFileTemplate(tokenName, fileName){
+    const deploymentFile =
+    `const ${tokenName} = artifacts.require("${fileName}");\n` +
+    `\n` +
+    `module.exports = function (deployer) {\n` +
+    `  deployer.deploy(${tokenName});\n` +
+    `};`;
+
+    return deploymentFile;
+}
+
+/**
+ * Creates a string for the contract file from a template and the parameters specified
+ * @param {*} name Name of the token
+ * @param {*} symbol Symbol of the token
+ * @param {*} tokenAmount The ammount of tokens
+ * @returns 
+ */
+function erc20ContractTemplate(name, symbol, tokenAmount){
+    const contract =
+    `// SPDX-License-Identifier: MIT\n` +
+    `pragma solidity ^0.8.9;\n` +
+    `\n` +
+    `import "@openzeppelin/contracts/token/ERC20/ERC20.sol";\n` +
+    `\n` +
+    `contract ${name} is ERC20 {\n` +
+    `    constructor() ERC20("${name}", "${symbol}") {\n` +
+    `        _mint(msg.sender, ${tokenAmount} * 10 ** decimals());\n` +
+    `    }\n` +
+    `}`;
+
+    return contract;
+}
+
 module.exports = {
     erc20Get,
     erc20Put,
     erc20Post,
     erc20Delete
-
 }
